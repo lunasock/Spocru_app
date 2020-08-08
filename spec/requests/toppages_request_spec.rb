6adc374c052ec8909533066a7cdacf647cd23236
TakKokubu <kokubu.tk@gmail.com>
2020-08-08 16:29:24 +0900

    rubocop指摘エラー箇所の修正

--- spec/requests/toppages_request_spec.rb
+++ spec/requests/toppages_request_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Toppages", type: :request do
+RSpec.describe 'Toppages', type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/toppages/index"
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/toppages/index'
       expect(response).to have_http_status(:success)
     end
   end
